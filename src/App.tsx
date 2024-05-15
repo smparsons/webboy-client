40ac4686150e4ece3f32ba16894138dd1e624cad
Samuel Parsons <samuel.m.parsons@gmail.com>
2024-05-15 08:26:18 -0400

    Move canvas code to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {
     FileBufferObject,
 } from "./components/bufferFileUpload";
 import { CssGrid, GapSize, Orientation, Position } from "./components/cssGrid";
+import GameScreen from "./components/gameScreen";
 import init from "./core/webboyCore";
 
 const AppGrid = styled(CssGrid)`
@@ -16,48 +17,13 @@ const InterfaceGrid = styled(CssGrid)`
     padding: 32px;
 `;
 
-const GAMEBOY_WIDTH = 160;
-const GAMEBOY_HEIGHT = 144;
-
-const Canvas = styled("canvas")`
-    width: ${GAMEBOY_WIDTH * 2}px;
-    height: ${GAMEBOY_HEIGHT * 2}px;
-`;
-
 const App = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
     const [wasmInitialized, setWasmInitialized] = useState(false);
 
     const [romBuffer, setRomBuffer] = useState(null as FileBufferObject | null);
     const [biosBuffer, setBiosBuffer] = useState(
         null as FileBufferObject | null,
     );
-
-    const initializeCanvas = (): void => {
-        const canvasElement = canvasRef.current;
-
-        if (canvasElement) {
-            const canvasContext = canvasElement.getContext("2d");
-
-            const initialBuffer = [];
-
-            for (let i = 0; i < GAMEBOY_WIDTH * GAMEBOY_HEIGHT; i++) {
-                const offset = i * 4;
-                initialBuffer[offset] = 0;
-                initialBuffer[offset + 1] = 0;
-                initialBuffer[offset + 2] = 0;
-                initialBuffer[offset + 3] = 0xff;
-            }
-
-            const data = new Uint8ClampedArray(initialBuffer);
-            const imageData = new ImageData(
-                data,
-                GAMEBOY_WIDTH,
-                GAMEBOY_HEIGHT,
-            );
-            canvasContext?.putImageData(imageData, 0, 0);
-        }
-    };
 
     const initalizeWasm = (): void => {
         init().then(() => {
@@ -68,12 +34,6 @@ const App = () => {
     useEffect(() => {
         initalizeWasm();
     }, []);
-
-    useEffect(() => {
-        if (wasmInitialized) {
-            initializeCanvas();
-        }
-    }, [wasmInitialized]);
 
     return (
         <AppGrid justifyContent={Position.center}>
@@ -90,12 +50,7 @@ const App = () => {
                                 A simple online Gameboy Emulator.
                             </Typography>
                         </div>
-                        <Canvas
-                            id="screen"
-                            width="160"
-                            height="144"
-                            ref={canvasRef}
-                        />
+                        <GameScreen wasmInitialized={wasmInitialized} />
                         <BufferFileUpload
                             label="Load ROM"
                             onFileSelect={setRomBuffer}
---
+++ src/components/gameScreen.tsx
@@ -0,0 +1,46 @@
+import { styled } from "@mui/material";
+import { useEffect, useRef } from "react";
+
+const GAMEBOY_WIDTH = 160;
+const GAMEBOY_HEIGHT = 144;
+
+const Screen = styled("canvas")`
+    width: ${GAMEBOY_WIDTH * 2}px;
+    height: ${GAMEBOY_HEIGHT * 2}px;
+`;
+
+const initializeCanvas = (canvas: HTMLCanvasElement): void => {
+    const canvasContext = canvas.getContext("2d");
+
+    const initialBuffer = [];
+
+    for (let i = 0; i < GAMEBOY_WIDTH * GAMEBOY_HEIGHT; i++) {
+        const offset = i * 4;
+        initialBuffer[offset] = 0;
+        initialBuffer[offset + 1] = 0;
+        initialBuffer[offset + 2] = 0;
+        initialBuffer[offset + 3] = 0xff;
+    }
+
+    const data = new Uint8ClampedArray(initialBuffer);
+    const imageData = new ImageData(data, GAMEBOY_WIDTH, GAMEBOY_HEIGHT);
+    canvasContext?.putImageData(imageData, 0, 0);
+};
+
+const GameScreen = ({ wasmInitialized }: GameScreenProps) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        if (wasmInitialized && canvasRef.current) {
+            initializeCanvas(canvasRef.current);
+        }
+    }, [wasmInitialized]);
+
+    return <Screen width="160" height="144" ref={canvasRef} />;
+};
+
+interface GameScreenProps {
+    wasmInitialized: boolean;
+}
+
+export default GameScreen;
