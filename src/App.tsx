4d1f4acdb7a6770c036ab5a18607d35cb50e6669
Samuel Parsons <samuel.m.parsons@gmail.com>
2024-05-17 07:53:30 -0400

    Add ability to start and play the emulator

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import { Button, Typography, styled } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import {
+    Button,
+    CssBaseline,
+    ThemeProvider,
+    Typography,
+    createTheme,
+    styled,
+} from "@mui/material";
+import { KeyboardEvent, useEffect, useRef, useState } from "react";
 
 import {
     BufferFileUpload,
@@ -7,7 +14,13 @@ import {
 } from "./components/bufferFileUpload";
 import { CssGrid, GapSize, Orientation, Position } from "./components/cssGrid";
 import GameScreen from "./components/gameScreen";
-import init from "./core/webboyCore";
+import init, {
+    initializeEmulator,
+    initializeEmulatorWithoutBios,
+    pressKey,
+    releaseKey,
+    stepFrame,
+} from "./core/webboyCore";
 
 const AppGrid = styled(CssGrid)`
     height: 100%;
@@ -17,6 +30,12 @@ const InterfaceGrid = styled(CssGrid)`
     padding: 32px;
 `;
 
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
+
 const App = (): JSX.Element => {
     const [wasmInitialized, setWasmInitialized] = useState(false);
 
@@ -25,53 +44,95 @@ const App = (): JSX.Element => {
         null as FileBufferObject | null,
     );
 
+    const [playing, setPlaying] = useState(false);
+
     const initalizeWasm = (): void => {
         init().then(() => {
             setWasmInitialized(true);
         });
     };
 
+    const playGame = (): void => {
+        if (biosBuffer && romBuffer) {
+            initializeEmulator(romBuffer.data, biosBuffer.data);
+        } else if (romBuffer) {
+            initializeEmulatorWithoutBios(romBuffer.data);
+        }
+
+        setPlaying(true);
+    };
+
     useEffect(() => {
         initalizeWasm();
     }, []);
 
+    const renderLoop = (): void => {
+        if (playing) {
+            stepFrame();
+            window.requestAnimationFrame(() => renderLoop());
+        }
+    };
+
+    useEffect(() => {
+        if (playing) {
+            window.onkeydown = e => {
+                pressKey(e.keyCode);
+            };
+
+            window.onkeyup = e => {
+                releaseKey(e.keyCode);
+            };
+
+            renderLoop();
+        }
+    }, [playing]);
+
     return (
-        <AppGrid justifyContent={Position.center}>
-            <InterfaceGrid
-                orientation={Orientation.vertical}
-                gap={GapSize.extraLarge}
-                justifyItems={Position.start}
-            >
-                {wasmInitialized ? (
-                    <>
-                        <div>
-                            <Typography variant="h3">WebBoy</Typography>
-                            <Typography variant="h6">
-                                A simple online Gameboy Emulator.
-                            </Typography>
-                        </div>
-                        <GameScreen wasmInitialized={wasmInitialized} />
-                        <BufferFileUpload
-                            label="Load ROM"
-                            onFileSelect={setRomBuffer}
-                            uploadedFile={romBuffer}
-                            variant="contained"
-                            accept=".gb"
-                        />
-                        <BufferFileUpload
-                            label="Load BIOS (Optional)"
-                            onFileSelect={setBiosBuffer}
-                            uploadedFile={biosBuffer}
-                            variant="contained"
-                            accept=".bin"
-                        />
-                        <Button variant="contained">Play Game</Button>
-                    </>
-                ) : (
-                    <div>Loading...</div>
-                )}
-            </InterfaceGrid>
-        </AppGrid>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <AppGrid justifyContent={Position.center}>
+                <InterfaceGrid
+                    orientation={Orientation.vertical}
+                    gap={GapSize.extraLarge}
+                    justifyItems={Position.start}
+                >
+                    {wasmInitialized ? (
+                        <>
+                            <div>
+                                <Typography variant="h3">WebBoy</Typography>
+                                <Typography variant="h6">
+                                    A simple online Gameboy Emulator.
+                                </Typography>
+                            </div>
+                            <GameScreen wasmInitialized={wasmInitialized} />
+                            <BufferFileUpload
+                                label="Load ROM"
+                                onFileSelect={setRomBuffer}
+                                uploadedFile={romBuffer}
+                                variant="contained"
+                                accept=".gb"
+                            />
+                            <BufferFileUpload
+                                label="Load BIOS (Optional)"
+                                onFileSelect={setBiosBuffer}
+                                uploadedFile={biosBuffer}
+                                variant="contained"
+                                accept=".bin"
+                            />
+                            <Button
+                                variant="contained"
+                                onClick={playGame}
+                                disabled={!romBuffer}
+                            >
+                                Play Game
+                            </Button>
+                        </>
+                    ) : (
+                        <div>Loading...</div>
+                    )}
+                </InterfaceGrid>
+            </AppGrid>
+        </ThemeProvider>
     );
 };
 
--- src/components/gameScreen.tsx
+++ src/components/gameScreen.tsx
@@ -7,12 +7,20 @@ const GAMEBOY_HEIGHT = 144;
 const Screen = styled("canvas")`
     width: ${GAMEBOY_WIDTH * 2}px;
     height: ${GAMEBOY_HEIGHT * 2}px;
+    border: ${({ theme }) => `1px solid ${theme.palette.text.secondary}`};
 `;
 
-const initializeCanvas = (canvas: HTMLCanvasElement): void => {
-    const canvasContext = canvas.getContext("2d");
+const renderFrame = (
+    canvasContext: CanvasRenderingContext2D,
+    buffer: number[],
+): void => {
+    const data = new Uint8ClampedArray(buffer);
+    const imageData = new ImageData(data, GAMEBOY_WIDTH, GAMEBOY_HEIGHT);
+    canvasContext.putImageData(imageData, 0, 0);
+};
 
-    const initialBuffer = [];
+const initializeCanvas = (canvasContext: CanvasRenderingContext2D): void => {
+    const initialBuffer = [] as number[];
 
     for (let i = 0; i < GAMEBOY_WIDTH * GAMEBOY_HEIGHT; i++) {
         const offset = i * 4;
@@ -22,9 +30,7 @@ const initializeCanvas = (canvas: HTMLCanvasElement): void => {
         initialBuffer[offset + 3] = 0xff;
     }
 
-    const data = new Uint8ClampedArray(initialBuffer);
-    const imageData = new ImageData(data, GAMEBOY_WIDTH, GAMEBOY_HEIGHT);
-    canvasContext?.putImageData(imageData, 0, 0);
+    renderFrame(canvasContext, initialBuffer);
 };
 
 const GameScreen = ({ wasmInitialized }: GameScreenProps): JSX.Element => {
@@ -32,7 +38,16 @@ const GameScreen = ({ wasmInitialized }: GameScreenProps): JSX.Element => {
 
     useEffect(() => {
         if (wasmInitialized && canvasRef.current) {
-            initializeCanvas(canvasRef.current);
+            const canvas = canvasRef.current;
+            const canvasContext = canvas.getContext("2d");
+
+            if (canvasContext) {
+                initializeCanvas(canvasContext);
+
+                (window as any).render = (buffer: number[]): void => {
+                    renderFrame(canvasContext, buffer);
+                };
+            }
         }
     }, [wasmInitialized]);
 
